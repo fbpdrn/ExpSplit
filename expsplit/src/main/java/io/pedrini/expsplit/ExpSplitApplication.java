426839c4d7237b34aa0cfadfe41b2c639eb3f249
Fabio Pedrini <fabio@pedrini.io>
2026-07-20 22:21:45 +0200

    initialize multi-module Spring Boot project

---
+++ expsplit/src/main/java/io/pedrini/expsplit/ExpSplitApplication.java
@@ -0,0 +1,13 @@
+package io.pedrini.expsplit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpSplitApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(ExpSplitApplication.class, args);
+    }
+
+}
